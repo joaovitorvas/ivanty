2ef25b407dd2f70eb8ced88466a3fe7730428bb6
João VItor Vasconcelos Lima <101337884+joaovitorvas@users.noreply.github.com>
2023-09-11 13:54:05 +0000

    pequenas atualizações

--- src/components/ArtistsContainer/index.jsx
+++ src/components/ArtistsContainer/index.jsx
@@ -1,9 +1,25 @@
-import { Container } from './styles';
+import { ArtistName, Container } from './styles';
+import { ArtistPicture } from './styles';
+import artist from './../../assets/images/artist.jpg'
+import { Artist } from './styles';
 
 export const ArtistsContainer = () => {
     return (
         <>
-           <Container/>
+           <Container>
+                <Artist>
+                    <ArtistPicture src={artist}/>
+                    <ArtistName>name</ArtistName>
+                </Artist>
+                <Artist>
+                    <ArtistPicture src={artist}/>
+                    <ArtistName>name</ArtistName>
+                </Artist>
+                <Artist>
+                    <ArtistPicture src={artist}/>
+                    <ArtistName>name</ArtistName>
+                </Artist>
+           </Container>
         </>
     );
   }
--- src/components/ArtistsContainer/styles.js
+++ src/components/ArtistsContainer/styles.js
@@ -4,4 +4,27 @@ export const Container = styled.div`
   background-color: #7B87C2;
   height: 10rem;
   width: 100%;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+
+`
+
+export const Artist = styled.div`
+`
+
+export const ArtistPicture = styled.img`
+  width: 80%;
+  margin: 1rem auto;
+  border-radius: 5rem;
+  text-align: center;
+`
+
+export const ArtistName = styled.span`
+  color: #FFF;
+  margin: 0 auto;
+  font-family: Roboto;
+  font-size: 1rem;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
 `
