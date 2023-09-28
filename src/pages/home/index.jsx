f37aa174c8218ad032e658f5939e68b9f1315233
João VItor Vasconcelos <101337884+joaovitorvas@users.noreply.github.com>
2023-09-28 21:16:52 +0000

    importando o fotter

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -2,11 +2,7 @@ import { Header } from '../../components/Header'
 import { Banner } from '../../components/Banner'
 import { MainContainer } from '../../components/Main';
 import banner from './../../assets/images/banner.png'
-import obra from './../../assets/images/campoDeTrigo.jpg'
-import { PinturasCard } from '../../components/PinturasCard';
-let title = 'Campo de trigo'
-let author = 'Vincent Van Gogh'
-let price = '799,99'
+import Footer from '../../components/Footer';
 
 
 const Home = () => {
@@ -15,6 +11,7 @@ const Home = () => {
       <Header/>
       <Banner imagem={banner}/>
       <MainContainer/>
+      <Footer/>
     </div>
   );
 }
